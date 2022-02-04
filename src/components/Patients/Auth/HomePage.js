6f9082b6ef2bd74f4040c47bc571480fc9efc195
Neel <indranil.swarnakar@gmail.com>
2022-02-04 16:31:11 +0530

    push neelwork

--- src/components/Patients/Auth/HomePage.js
+++ src/components/Patients/Auth/HomePage.js
@@ -10,21 +10,71 @@ import AxiosFunction from '../AxiosFunction';
 
 const HomePage = (props) => {
 
-    axios.get(`${process.env.REACT_APP_ENV}/api/get-banner`).then(resp=>{
-        console.log('Axios Data----->>>>',resp.data)
-    })
+
+    const [banners, setBanners] = useState([]);
+
+    const [specialiest, setSpecialiest] = useState([]);
+
+    const [issues, setIssues] = useState([])
+    const [loader, setLoader] = useState(true)
+    // api/get-specialty', views.getSpecialty, name='get-specialty'),
+    // path('api/get-health-issues
+
+
+    
+
+
+    useEffect(() => {
+        let mounted = true;
+        if (mounted) {
+
+
+            axios.get(`${process.env.REACT_APP_ENV}/api/get-banner`).then(resp=>{
+                //console.log('Axios Data----->>>>',resp.data.data)
+                setBanners(resp.data.data)
+            })
+
+            axios.get(`${process.env.REACT_APP_ENV}/api/get-specialty`).then(resp=>{
+                console.log('Axios Data Speciliest----->>>>',resp.data)
+                //setBanners(resp.data.data)
+            })
+
+            axios.get(`${process.env.REACT_APP_ENV}/api/get-health-issues`).then(resp=>{
+                console.log('Axios Data Issues----->>>>',resp.data)
+                //setBanners(resp.data.data)
+                setLoader(false)
+            })
+
+        }
+        return () => mounted = false;
+        }, [])
 
 
 
-    const [loader, setLoader] = useState(false)
+    
     return (
         <LoadingOverlay active={loader} spinner>
             <ToastContainer />
             <div className="container-scroller">
 
+                
+
                 <Nav/>
 
                     <div className="container-fluid page-body-wrapper">
+
+                       
+                       
+
+                    {banners.map((i,id)=>(
+
+                    <p key={id}> 
+                        Id: {i.id} 
+                        Name: {i.name} 
+                        <br/>
+                        
+                        </p>
+                        ))}
 
                     </div>
                     
